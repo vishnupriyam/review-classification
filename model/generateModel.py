7ee747d8e5a439ecaf998fa4a2da0c5b9ef4228f
vishnupriyam <mvpnov1994@gmail.com>
2016-04-05 10:49:19 +0530

    generatemodel converted to package

--- model/generateModel.py
+++ model/generateModel.py
@@ -7,26 +7,29 @@ import pickle
 
 from train import train_multinomial_naive_bayes
 
-check = len(sys.argv)
-if(check > 1):
-    reviewfile = sys.argv[1]
-    vocabfile = sys.argv[2]
-else:
+def generatemodel(reviewfile,vocabfile,paramfile):
+
+    freview = open(reviewfile,"r")
+    fvocab  = open(vocabfile,"r")
+
+    reviews = []
+    vocabulary = []
+    for line in freview:
+        reviews.append(line.rstrip('\n'))
+    for line in fvocab:
+        vocabulary.append(line.rstrip('\n'))
+
+    (PP,PN,positive_probabilities,negative_probabilities,unseen_pos_prob,unseen_neg_prob) = train_multinomial_naive_bayes(reviews,vocabulary)
+
+    training_model = (PP,PN,positive_probabilities,negative_probabilities,unseen_pos_prob,unseen_neg_prob)
+
+    pickle.dump(training_model,open(paramfile,"w"))
+
+def main():
     reviewfile = "../cleaned-data/stopwords-removed-data-nltk.txt"
-    vocabfile  = "../vocabulary/vocabulary2.txt"
+    vocabfile = "../vocabulary/vocabulary2.txt"
+    paramfile = "parameters.p"
+    generatemodel(reviewfile, vocabfile,paramfile)
 
-freview = open(reviewfile,"r")
-fvocab  = open(vocabfile,"r")
-
-reviews = []
-vocabulary = []
-for line in freview:
-    reviews.append(line.rstrip('\n'))
-for line in fvocab:
-    vocabulary.append(line.rstrip('\n'))
-
-(PP,PN,positive_probabilities,negative_probabilities,unseen_pos_prob,unseen_neg_prob) = train_multinomial_naive_bayes(reviews,vocabulary)
-
-training_model = (PP,PN,positive_probabilities,negative_probabilities,unseen_pos_prob,unseen_neg_prob)
-
-pickle.dump(training_model,open("parameters.p","w"))
+if __name__ == "__main__":
+    main()
